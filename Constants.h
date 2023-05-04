11da00684dcb82166498f7fff0152a70d8a9f015
CarneiroGabriel <rodriguesgabriel@edu.univali.br>
2023-05-03 21:14:30 -0300

    Atualizacao no semantico

--- Constants.h
+++ Constants.h
@@ -90,7 +90,7 @@ extern int SPECIAL_CASES_VALUES[29];
 
 extern const char *SCANNER_ERROR[STATES_COUNT];
 
-const int FIRST_SEMANTIC_ACTION = 127;
+const int FIRST_SEMANTIC_ACTION = 129;
 
 const int SHIFT  = 0;
 const int REDUCE = 1;
@@ -99,10 +99,10 @@ const int ACCEPT = 3;
 const int GO_TO  = 4;
 const int ERROR  = 5;
 
-extern const int PARSER_TABLE[252][126][2];
+extern const int PARSER_TABLE[319][165][2];
 
-extern const int PRODUCTIONS[132][2];
+extern const int PRODUCTIONS[137][2];
 
-extern const char *PARSER_ERROR[252];
+extern const char *PARSER_ERROR[319];
 
 #endif
